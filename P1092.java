1c5bb2b0d3c25e7b6ee37991589561ab7b9e4581
chueunjeong <chu.wips@gmail.com>
2022-10-24 13:01:08 +0900

    20221024 bj1092 complete

--- P1092.java
+++ P1092.java
@@ -9,16 +9,18 @@ public class P1092 {
 
         try (Scanner sc = new Scanner(System.in)) {
             //크레인 수
-            int craneNum=sc.nextInt();
+            int craneNum=Integer.parseInt(sc.nextLine());
 
-            //크레인 제한 무게 목록
+           
+             //크레인 제한 무게 목록
             String craneLimitWeightStr=sc.nextLine();
-            int[] craneLimitWeightList=Stream.of(craneLimitWeightStr.split(" ")).mapToInt(Integer::parseInt).toArray();
-            
-            //박스 수
-            int boxesNum=sc.nextInt();
 
-            //박스 무게 목록
+             int[] craneLimitWeightList=Stream.of(craneLimitWeightStr.split(" ")).mapToInt(Integer::parseInt).toArray();
+            
+            //박스 수           
+            int boxesNum=Integer.parseInt(sc.nextLine());
+
+            //박스 무게 목록           
             String boxesWeightListStr=sc.nextLine();
             int[] boxesWeightList=Stream.of(boxesWeightListStr.split(" ")).mapToInt(Integer::parseInt).toArray();
 
@@ -30,8 +32,10 @@ public class P1092 {
 
     public static int getNumberOfMoves(int[] craneList, int[] boxWeightList){
         
-        //먼저, 크레인과 박스들 정렬
-        Arrays.sort(craneList);
+        //먼저, 크레인(내림차순)과 박스들(오름차순) 정렬
+        Integer[] boxedCraneList = Arrays.stream( craneList ).boxed().toArray( Integer[]::new );
+        Arrays.sort(boxedCraneList,Collections.reverseOrder());
+
         Arrays.sort(boxWeightList);
         
 
@@ -44,9 +48,24 @@ public class P1092 {
         //이동 횟수용 변수
         int moveCount=0;
 
-        
-        
-        return 0;
+        //나오는 장치
+        Boolean breakKey=true;
 
+        if(STACK.peek()>boxedCraneList[0]) return -1;
+        
+        while(breakKey){
+            for (Integer crane : boxedCraneList) {
+                if(STACK.empty()) break;
+
+                if(STACK.peek()<=crane) STACK.pop();
+                else break;
+            }
+
+            moveCount++;
+            
+            if(STACK.empty()) breakKey=false;
+        }        
+        
+        return moveCount;
     }
 }
